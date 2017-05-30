895428e8c1520784607ec3df62e97ee835b558ef
Supersonic78 <rikard.holmgren@gmail.com>
2017-05-30 16:04:07 +0200

    exercise3

---
+++ TDD-test2/StringCalculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public static int Add(string value)
+        {
+            if (value == "") return 0;
+            var sum = 0;
+            var delimiters = new List<char> { ',', '\n' };
+            if (value.StartsWith("//"))
+            {
+                delimiters.Add(value[2]);
+                value = value.Substring(4);
+            }
+            var values = new List<int>();
+            Array.ForEach(value.Split(delimiters.ToArray()), s => values.Add (int.Parse(s)));
+
+            values.ForEach(s => sum += s);
+            return sum;
+        }
+    }
+}
---
+++ TDD-test2/StringCalculatorTests/StringCalculatorTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using StringCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorTests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+
+        [Test]
+        public void AddEmptyStringReturnZero()
+        {
+            var result = Calculator.Add("");
+            Assert.AreEqual(0, result);
+        }
+        [Test]
+        public void AddOneNumberReturnsNumber()
+        {
+            var result = Calculator.Add("1");
+            Assert.AreEqual(1, result);
+        }
+        [Test]
+        public void Add_TwoNumbersSeparatedByDelimiter_ReturnsSum()
+        {
+            var result = Calculator.Add("1,2");
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Add_MultipleNumbersSeparatedByDelimiter_ReturnsSum()
+        {
+            var result = Calculator.Add("1,2,3,4");
+            Assert.AreEqual(10, result);
+        }
+        [Test]
+        public void SplittNumbersOnNewLine()
+        {    
+            var result = Calculator.Add("1\n2,3");
+            Assert.AreEqual(6, result);
+        }
+       
+        [Test]
+        public void AddNegativeNumbers_CastExeption()
+        {
+     
+        }
+    }
+}
